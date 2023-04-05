e2c376df0cdc8e7a18b7cd63da76e842bc4d1f52
dsadhu <dsadhu@ncsu.edu>
2023-04-04 21:54:09 -0400

    playing w/ diff methods of file reading

---
+++ index.js
@@ -0,0 +1,28 @@
+//const {readFile, readFileSync} = require('fs')
+
+
+// do this asap is printed last
+// const txt = readFileSync('./hello.txt', 'utf8')
+// console.log(txt)
+// console.log("DO THIS ASAP")
+
+
+//refactor above implementation with callback to make sure do this asap is printed first
+// readFile('./hello.txt', 'utf8', (err,txt2) => {
+//     console.log(txt2);
+// });
+// console.log("DO THIS ASAP")
+
+
+
+//promise based solution for file reading
+
+//prommises are async and nonblocking ... tend to produce cleaner code vs callbacks
+
+//this returns a promise when called
+const{readFile} = require('fs').promises;
+
+async function hello(){
+    const file = await readFile('./hello.txt', 'utf8');
+    
+}
